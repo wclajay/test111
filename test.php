89b1f13237da404bbcd3d2f7cd97ed5001109ed1
wclajay <ajaybahaduar@williamscommerce.com>
2014-12-22 17:03:00 +0530

    ny commyt

---
+++ test.php
@@ -0,0 +1,33 @@
+<?php 
+$osTax=100;
+$oFsTax = $osTax*.2;
+echo $oFsTax;die;
+
+$a=explode('-','Product name-Product name');
+echo $a[1];
+
+die;
+
+
+$n=0;
+for($i=1;$i<4;$i++){
+$n +=$i;
+
+}
+
+
+
+$creditlimit=-323;
+if($creditlimit<0){
+ $creditlimitN=explode('-',$creditlimit);
+ echo $creditlimitN[0].$creditlimitN[1];
+ }
+?>
+
+<input id="creditlimit" value="creditlimit" type="radio" name="payment[method]" title="" onclick="checkcreditlimit()" class="radio" />
+<script language="javascript1.1">
+function checkcreditlimit(){
+	alert("You don't have enough balance");
+	return false;
+	}
+</script>
